6c6dcbef30ee7e90a47ac9ac6621005e02722a9c
twilroad <269044570@qq.com>
2017-09-19 10:35:24 +0800

    add definition of service provider

---
+++ src/Extension.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <heshudong@ibenchu.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-09-19 10:32
+ */
+namespace Notadd\Neditor;
+
+use Notadd\Foundation\Extension\Abstracts\Extension as AbstractExtension;
+
+/**
+ * Class Extension.
+ */
+class Extension extends AbstractExtension
+{
+    /**
+     * Boot extension.
+     */
+    public function boot()
+    {
+        // TODO: Implement boot() method.
+    }
+
+    /**
+     * Installer for extension.
+     *
+     * @return string
+     */
+    public static function install()
+    {
+        // TODO: Implement install() method.
+    }
+
+    /**
+     * Uninstall for extension.
+     *
+     * @return string
+     */
+    public static function uninstall()
+    {
+        // TODO: Implement uninstall() method.
+    }
+}
